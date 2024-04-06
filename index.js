16ad82b3841501479a7575d96254fc2cbe1a06ef
Tim <128478437+timcharlier6@users.noreply.github.com>
2024-04-06 17:06:11 +0200

    update

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const fs = require('fs')
+const generateRandomHex = require('./randomId.js')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true }))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', (req, res) => {
+    const user_id = generateRandomHex(24)
+    const newUser = req.body.username
 
+    let userData = []
+
+    try {
+        userData = JSON.parse(fs.readFileSync('users.json'))
+    } catch (error) {
+        console.error('Error reading user data', error)
+    }
+
+    userData.push({ username: newUser, _id: user_id })
+    const newUserData = userData.filter(user => user.username === newUser);
+    console.log(userData)
+
+    fs.writeFile('users.json', JSON.stringify(userData, null, 2), err => {
+        if (err) {
+            console.error('Error writing user data', err)
+            res.status(500).send('Error saving user')
+        } else {
+            res.send(newUserData)
+        }
+    })
+})
 
 
 
---
+++ randomId.js
@@ -0,0 +1,11 @@
+function generateRandomHex(length) {
+  const characters = '0123456789abcdef';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+module.exports = generateRandomHex;
+
